d8febbdcaa1b3874b325528296f924fadac87baa
Sergey Solodukhin <soloss@yandex.ru>
2017-03-25 21:33:25 +0300

    Added knight's gePseudoLegalMove method

--- app/pieces/knight.ts
+++ app/pieces/knight.ts
@@ -1,4 +1,4 @@
-import {MoveData, IPiece} from './piece';
+import { MoveData, IPiece, MoveFlags } from './piece';
 import Position from '../position';
 
 export default class Knight implements IPiece {
@@ -15,6 +15,18 @@ export default class Knight implements IPiece {
     fromRow: number, fromColumn: number,
     toRow: number, toColumn: number): MoveData 
   {
-    return null;
+    let istep = Math.abs(toRow - fromRow);
+    let jstep = Math.abs(toColumn - fromColumn);
+    if (istep == 0 || jstep == 0 || istep + jstep != 3) return null;
+
+    let moveData = new MoveData(fromRow, fromColumn, toRow, toColumn);
+    let captured = pos.position[toRow][toColumn];
+    if (captured != null) {
+      // can't capture own piece
+      if (this.isWhite === captured.isWhite) return null;
+        moveData.flags = MoveFlags.Capture,
+        moveData.capturedPiece = captured;
+    }
+    return moveData;      
   }
 }
--- spec/piece-move.spec.ts
+++ spec/piece-move.spec.ts
@@ -2,124 +2,126 @@ import Position from '../app/position';
 import Inspector from '../app/inspector';
 import { MoveFlags } from "../app/pieces/piece";
 
-describe("The pawn has five legal moves:", function() {
+describe("The pawn has five legal moves:", 
+  function() {
 
-  let whitesInspector: Inspector; 
-  let blacksInspector: Inspector;
+    let whitesInspector: Inspector; 
+    let blacksInspector: Inspector;
 
-  beforeAll(function () {
-    whitesInspector = new Inspector(new Position("6k1/3pp3/8/4Pp2/5Pp1/5P1p/3P2PP/3K4 w - f"));
-    blacksInspector = new Inspector(new Position("8/2ppp3/4Pp2/2P5/R4Ppk/8/8/3K4 b - f"));
-  });
+    beforeAll(function () {
+      whitesInspector = new Inspector(new Position("6k1/3pp3/8/4Pp2/5Pp1/5P1p/3P2PP/3K4 w - f"));
+      blacksInspector = new Inspector(new Position("8/2ppp3/4Pp2/2P5/R4Ppk/8/8/3K4 b - f"));
+    });
 
-  describe(
-    `a) The pawn moves forward to the unoccupied square 
-    immediately in front of it on the same file.`, 
-    function() {
-      it("Inspector return valid data.", function() {
-        let moveData = whitesInspector.getMove("d2", "d3");
-        expect(moveData).not.toBeNull();
-        expect(moveData.flags).toBe(MoveFlags.Quiet);
-        moveData = blacksInspector.getMove("d7", "d6");
-        expect(moveData).not.toBeNull();
-        expect(moveData.flags).toBe(MoveFlags.Quiet);
-      });
-           
-      it("Try to move backward fail.", function() {
-        expect(whitesInspector.getMove("d5", "d4")).toBeNull();
-        expect(blacksInspector.getMove("g4", "g5")).toBeNull();
-      });
+    describe(
+      `a) The pawn moves forward to the unoccupied square 
+      immediately in front of it on the same file.`, 
+      function() {
+        it("Inspector return valid data.", function() {
+          let moveData = whitesInspector.getMove("d2", "d3");
+          expect(moveData).not.toBeNull();
+          expect(moveData.flags).toBe(MoveFlags.Quiet);
+          moveData = blacksInspector.getMove("d7", "d6");
+          expect(moveData).not.toBeNull();
+          expect(moveData.flags).toBe(MoveFlags.Quiet);
+        });
+            
+        it("Try to move backward fail.", function() {
+          expect(whitesInspector.getMove("d5", "d4")).toBeNull();
+          expect(blacksInspector.getMove("g4", "g5")).toBeNull();
+        });
 
-      it("Try to move on occuped square fail.", function() {
-        expect(whitesInspector.getMove("f4", "f5")).toBeNull();
-        expect(blacksInspector.getMove("e7", "e6")).toBeNull();
-      });
-  });
+        it("Try to move on occuped square fail.", function() {
+          expect(whitesInspector.getMove("f4", "f5")).toBeNull();
+          expect(blacksInspector.getMove("e7", "e6")).toBeNull();
+        });
+    });
 
-  describe(
-    `b) On its first move each pawn may advance two squares along the same file 
-    provided both squares are unoccupied.`,
-    function() {
-      it("Inspector return valid data.", function() {
-        let moveData = whitesInspector.getMove("d2", "d4");
-        expect(moveData).not.toBeNull();
-        expect(moveData.flags).toBe(MoveFlags.Quiet);
-        moveData = blacksInspector.getMove("d7", "d5");
-        expect(moveData).not.toBeNull();
-        expect(moveData.flags).toBe(MoveFlags.Quiet);
-      });
+    describe(
+      `b) On its first move each pawn may advance two squares along the same file 
+      provided both squares are unoccupied.`,
+      function() {
+        it("Inspector return valid data.", function() {
+          let moveData = whitesInspector.getMove("d2", "d4");
+          expect(moveData).not.toBeNull();
+          expect(moveData.flags).toBe(MoveFlags.Quiet);
+          moveData = blacksInspector.getMove("d7", "d5");
+          expect(moveData).not.toBeNull();
+          expect(moveData.flags).toBe(MoveFlags.Quiet);
+        });
 
-      it("Try to move over obstacle fail.", function() {
-        expect(whitesInspector.getMove("h2", "h4")).toBeNull();
-        expect(blacksInspector.getMove("e7", "e5")).toBeNull();
-      });
-      
-      it("Try to move on occuped square fail.", function() {
-        expect(whitesInspector.getMove("g2", "g4")).toBeNull();
-        expect(blacksInspector.getMove("c7", "c5")).toBeNull();
-      });
-  });
+        it("Try to move over obstacle fail.", function() {
+          expect(whitesInspector.getMove("h2", "h4")).toBeNull();
+          expect(blacksInspector.getMove("e7", "e5")).toBeNull();
+        });
+        
+        it("Try to move on occuped square fail.", function() {
+          expect(whitesInspector.getMove("g2", "g4")).toBeNull();
+          expect(blacksInspector.getMove("c7", "c5")).toBeNull();
+        });
+    });
 
-  describe(
-    `c) Capture: the pawn moves to a square occupied by an opponent’s
-    piece which is diagonally in front of it on an adjacent file, capturing that piece.`,
-    function() {
-      it("Inspector return valid data.", function() {
-        let moveData = whitesInspector.getMove("f3", "g4");
-        expect(moveData).not.toBeNull();
-        expect(moveData.flags).toBe(MoveFlags.Capture);
-        expect(moveData.capturedPiece).not.toBeNull;
+    describe(
+      `c) Capture: the pawn moves to a square occupied by an opponent’s
+      piece which is diagonally in front of it on an adjacent file, capturing that piece.`,
+      function() {
+        it("Inspector return valid data.", function() {
+          let moveData = whitesInspector.getMove("f3", "g4");
+          expect(moveData).not.toBeNull();
+          expect(moveData.flags).toBe(MoveFlags.Capture);
+          expect(moveData.capturedPiece).not.toBeNull;
 
-        moveData = blacksInspector.getMove("d7", "e6");
-        expect(moveData).not.toBeNull();
-        expect(moveData.flags).toBe(MoveFlags.Capture);
-        expect(moveData.capturedPiece).not.toBeNull;
-      });
+          moveData = blacksInspector.getMove("d7", "e6");
+          expect(moveData).not.toBeNull();
+          expect(moveData.flags).toBe(MoveFlags.Capture);
+          expect(moveData.capturedPiece).not.toBeNull;
+        });
 
-      it("Try to capture own piece fail.", function() {
-        expect(whitesInspector.getMove("g2", "f3")).toBeNull();
-        expect(blacksInspector.getMove("e7", "f6")).toBeNull();
-      });
+        it("Try to capture own piece fail.", function() {
+          expect(whitesInspector.getMove("g2", "f3")).toBeNull();
+          expect(blacksInspector.getMove("e7", "f6")).toBeNull();
+        });
 
-      it("Try to move like capture on empty square fail.", function() {
-        expect(whitesInspector.getMove("d2", "c3")).toBeNull();
-        expect(blacksInspector.getMove("g4", "h3")).toBeNull();
-      });
-  });
+        it("Try to move like capture on empty square fail.", function() {
+          expect(whitesInspector.getMove("d2", "c3")).toBeNull();
+          expect(blacksInspector.getMove("g4", "h3")).toBeNull();
+        });
+    });
 
-  describe(
-    `d) Capture en-passant: a pawn attacking a square crossed by an opponent’s pawn
-    which has advanced two squares in one move from its original square
-    may capture this opponent’s pawn as though the latter had been
-    moved only one square. This capture can be made only on the move
-    following this advance.`,
-    function() {
-      it("Inspector return valid data.", function() {
-        let moveData = whitesInspector.getMove("e5", "f6");
-        expect(moveData).not.toBeNull();
-        expect(moveData.flags).toBe(MoveFlags.CaptureEnPassant);
-        expect(moveData.capturedPiece).not.toBeNull;
+    describe(
+      `d) Capture en-passant: a pawn attacking a square crossed by an opponent’s pawn
+      which has advanced two squares in one move from its original square
+      may capture this opponent’s pawn as though the latter had been
+      moved only one square. This capture can be made only on the move
+      following this advance.`,
+      function() {
+        it("Inspector return valid data.", function() {
+          let moveData = whitesInspector.getMove("e5", "f6");
+          expect(moveData).not.toBeNull();
+          expect(moveData.flags).toBe(MoveFlags.CaptureEnPassant);
+          expect(moveData.capturedPiece).not.toBeNull;
 
-        moveData = blacksInspector.getMove("g4", "f3");
-        expect(moveData).not.toBeNull();
-        expect(moveData.flags).toBe(MoveFlags.CaptureEnPassant);
-        expect(moveData.capturedPiece).not.toBeNull;
-      });
-  });  
-  // TODO: check en-passant flag toggle
+          moveData = blacksInspector.getMove("g4", "f3");
+          expect(moveData).not.toBeNull();
+          expect(moveData.flags).toBe(MoveFlags.CaptureEnPassant);
+          expect(moveData.capturedPiece).not.toBeNull;
+        });
+    });  
+    // TODO: check en-passant flag toggle
 
-  describe(
-    `e) Promotion: when a pawn reaches the rank furthest from its starting position
-    it must be exchanged as part of the same move for a queen,
-    rook, bishop or knight of the same color.`,
-    function() {
+    describe(
+      `e) Promotion: when a pawn reaches the rank furthest from its starting position
+      it must be exchanged as part of the same move for a queen,
+      rook, bishop or knight of the same color.`,
+      function() {
 
-  });
+    });
 });
 
 describe(`The queen moves to any square along the file, the rank
   or a diagonal on which it stands. When making these moves the queen cannot
-  move over any intervening pieces.`, function() {
+  move over any intervening pieces.`, 
+  function() {
 
     let inspector: Inspector;
 
@@ -156,7 +158,8 @@ describe(`The queen moves to any square along the file, the rank
 
 describe(`The rook moves to any square along the file or the rank
   on which it stands. When making these moves the rook cannot
-  move over any intervening pieces.`, function() {
+  move over any intervening pieces.`, 
+  function() {
 
     let inspector: Inspector;
 
@@ -189,7 +192,7 @@ describe(`The rook moves to any square along the file or the rank
 
 describe(`The bishop moves to any square along a diagonal on which it stands. 
   When making these moves the bishop cannot move over any intervening pieces.`, 
-    function() {
+  function() {
 
     let inspector: Inspector;
 
@@ -217,5 +220,34 @@ describe(`The bishop moves to any square along a diagonal on which it stands.
 
     it("Capture own pieces fail.", function() {
       expect(inspector.getMove("d3", "c2")).toBeNull();
+    }); 
+});
+
+describe(`The knight moves to one of the squares nearest to that on
+  which it stands but not on the same rank, file or diagonal.`, 
+  function() {
+
+    let inspector: Inspector;
+
+    beforeAll(function(){
+      inspector = new Inspector(new Position("3k4/8/8/8/2P1p3/8/3N4/3K4 w - -"));
+    });
+
+    it("Inspector return valid data for valid move.", function() {
+      let moveData = inspector.getMove("d2", "f3");
+      expect(moveData).not.toBeNull();
+      expect(moveData.flags).toBe(MoveFlags.Quiet); 
+
+      moveData = inspector.getMove("d2", "e4");
+      expect(moveData).not.toBeNull();
+      expect(moveData.flags).toBe(MoveFlags.Capture);    
+    });
+
+    it("Incorrect move fail.", function() {
+      expect(inspector.getMove("d2", "d4")).toBeNull();
+    }); 
+
+    it("Capture own pieces fail.", function() {
+      expect(inspector.getMove("d2", "c4")).toBeNull();
     }); 
 });
